9c303d817772db9480a06890cad1c2016861ae0f
Randall Mardus <randalljmardus@gmail.com>
2016-03-17 17:41:03 -0400

    setup tableview

---
+++ WhaleTalk/ChatViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  WhaleTalk
+//
+//  Created by Randall Mardus on 3/17/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    private let tableView = UITableView()
+    
+    private var messages = [Message]()
+    private let cellIdentifier = "Cell"
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        for i in 0...10 {
+            let m = Message()
+            m.text = String(i)
+            messages.append(m)
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
+        tableView.dataSource = self
+            
+            
+        }
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        let tableViewConstraints: [NSLayoutConstraint] = [
+        
+            tableView.topAnchor.constraintEqualToAnchor(view.topAnchor),
+            tableView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor),
+            tableView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+            tableView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor)
+    
+        ]
+        
+        NSLayoutConstraint.activateConstraints(tableViewConstraints)
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        let message = messages[indexPath.row]
+        cell.textLabel?.text = message.text
+        return cell
+        
+    }
+}
+
+
+
+
+
+
+
+
---
+++ WhaleTalk/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  WhaleTalk
+//
+//  Created by Randall Mardus on 3/17/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    var text: String?
+}
